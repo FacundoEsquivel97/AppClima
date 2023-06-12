6c4078210cc0e5783d0f92c505e35101dd1008dc
FacundoEsquivel97 <esquivelfacundo.97@gmail.com>
2023-06-12 10:43:24 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Container, Typography } from "@mui/material";
 import { useState } from "react";
 import Card from "./components/Card";
 import Credits from "./components/Credits";
-const API_WEATHER =  `https://api.weatherapi.com/v1/current.json?key=3f2f34fd213a4c5e95e131848231206&lang=es&q=`;
+const API_WEATHER =  `https://api.weatherapi.com/v1/current.json?key=${import.meta.env.VITE_API_KEY}&lang=es&q=`;
 
 export default function App() {
 
